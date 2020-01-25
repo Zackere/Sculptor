ad411157bd00e5a958139a2c572e98ba086c8ac2
Zackere <wo1001@wp.pl>
2020-01-25 14:51:15 +0100

    Add main

--- sculptor/sculptor.cpp
+++ sculptor/sculptor.cpp
@@ -1,8 +1,5 @@
-#include "../include/sculptor.hpp"
+#include "./sculptor.hpp"
 
-#include "../include/drill.hpp"
-#include "../include/kdtree_cpu.hpp"
-#include "../include/sculpting_material.hpp"
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
@@ -33,44 +30,19 @@ int Sculptor::Main() {
   glEnable(GL_CULL_FACE);
   glCullFace(GL_BACK);
 
-  constexpr float side_len = 100;
-  SculptingMaterial material(SculptingMaterial::MaterialType::CUBE,
-                             SculptingMaterial::InitialShape::CUBE, side_len,
-                             std::make_unique<KdTreeCPU>());
-  Drill drill;
-
   glm::mat4 projection = glm::perspective(
       glm::radians(45.0f), static_cast<float>(wWidth) / wHeight, 0.1f, 100.0f);
   glm::mat4 view =
       glm::lookAt(glm::vec3(3, 0, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-  auto vp = projection * view;
+  [[maybe_unused]] auto vp = projection * view;
 
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
   glClearColor(44.0f / 255.0f, 219.0f / 255.0f, 216.0f / 255.0f, 0.0f);
 
   do {
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-      material.Rotate(-0.01f);
-    else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-      material.Rotate(0.01f);
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-      drill.MoveForward();
-    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-      drill.MoveBackward();
-    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-      drill.MoveUp();
-    else if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-      drill.MoveDown();
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    material.Render(vp);
-    drill.Render(vp);
-
     glfwSwapBuffers(window);
-
-    material.Collide(drill);
-    drill.NextFrame();
 
     glfwPollEvents();
   } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
