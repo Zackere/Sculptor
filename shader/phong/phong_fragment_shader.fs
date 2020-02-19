cd49bc4f2dfa3bd1a72439ed28d6f0795a18aec2
Zackere <wo1001@wp.pl>
2020-02-19 12:57:35 +0100

    Refactor phong fragment shader

--- shader/phong/phong_fragment_shader.fs
+++ shader/phong/phong_fragment_shader.fs
@@ -20,23 +20,33 @@ in vec2 uv;
 
 out vec3 color;
 
+vec3 CalculateDirectionalLightContribution(vec3 normal, vec3 eye_dir);
+
 void main(){
     vec3 n = normalize(normal);
     vec3 eye_dir = normalize(eye_pos - pos);
 
     color = vec3(0, 0, 0);
-    for(int i = 0; i < NDIRECTIONALLIGHTS; ++i){
+
+    color += CalculateDirectionalLightContribution(n, eye_dir);
+
+    color *= texture(texture_sampler, uv).rgb;
+    clamp(color, 0.0, 1.0);
+}
+
+vec3 CalculateDirectionalLightContribution(vec3 normal, vec3 eye_dir) {
+    vec3 ret = vec3(0, 0, 0);
+    for(int i = 0; i < NDIRECTIONALLIGHTS; ++i) {
         if(!SculptorDirectionalLight[i].enabled)
             continue;
 
         vec3 light = normalize(SculptorDirectionalLight[i].direction);
-        float diff_cos = max(dot(light, n), 0.0);
-        float spec_cos = pow(max(dot(2 * diff_cos * n - light, eye_dir), 0.0), light_coefficient.w);
+        float diff_cos = max(dot(light, normal), 0.0);
+        float spec_cos = pow(max(dot(2 * diff_cos * normal - light, eye_dir), 0.0), light_coefficient.w);
 
-        color += light_coefficient.x * SculptorDirectionalLight[i].ambient
-               + light_coefficient.y * SculptorDirectionalLight[i].diffuse * diff_cos
-               + light_coefficient.z * SculptorDirectionalLight[i].specular * (diff_cos > 0 ? spec_cos : 0);
+        ret += light_coefficient.x * SculptorDirectionalLight[i].ambient
+             + light_coefficient.y * SculptorDirectionalLight[i].diffuse * diff_cos
+             + light_coefficient.z * SculptorDirectionalLight[i].specular * (diff_cos > 0 ? spec_cos : 0);
     }
-    color *= texture(texture_sampler, uv).rgb;
-    clamp(color, 0.0, 1.0);
+    return ret;
 }
